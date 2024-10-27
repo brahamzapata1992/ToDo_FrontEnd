574e08eb0e24ea7ec07eaaf085244112cc7e14a9
brahamzapata1992 <brahamzapata1992@gmail.com>
2024-10-27 18:18:53 -0400

    estilos pagina principal

--- src/components/UserRegistration.jsx
+++ src/components/UserRegistration.jsx
@@ -1,11 +1,24 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
-import './UserRegistration.css'
+import './UserRegistration.css';
 
 const UserRegistration = () => {
   const [name, setName] = useState('');
   const navigate = useNavigate();
+
+  // Mover la declaración de images fuera de handleRegister
+  const images = [
+    '/src/assets/avatars/Av1.png',
+    '/src/assets/avatars/Av2.png',
+    '/src/assets/avatars/Av3.png',
+    '/src/assets/avatars/Av4.png',
+    '/src/assets/avatars/Av5.png',
+    '/src/assets/avatars/Av6.png',
+    '/src/assets/avatars/Av7.png',
+    '/src/assets/avatars/Av8.png',
+    '/src/assets/avatars/Av9.png',
+  ];
 
   const handleRegister = async () => {
     const formData = new FormData();
@@ -25,17 +38,25 @@ const UserRegistration = () => {
 
   return (
     <div className='Container_principal'>
-        <div className='Container_formulario'>
-            <h2>Registro de Usuario</h2>
-             <input
-                type="text"
-                placeholder="Ingresa tu nombre"
-                value={name}
-                onChange={(e) => setName(e.target.value)}
-            />
-            <button onClick={handleRegister}>Registrar</button>
+      <h1 className='titulo_app'>TaskFlow</h1>
+      <div className='Container_formulario'>
+        <h2 className='title_form'>Register Your Name</h2>
+        <input
+          className='input_form'
+          type="text"
+          placeholder="David Mathew"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <div className="gallery-container">
+          {images.map((image, index) => (
+            <div key={index} className="gallery-item">
+              <img src={image} alt={`Image ${index + 1}`} />
+            </div>
+          ))}
         </div>
-        <h1>hola</h1>      
+        <button className='botton_form' onClick={handleRegister}>Registrar</button>
+      </div>
     </div>
   );
 };
